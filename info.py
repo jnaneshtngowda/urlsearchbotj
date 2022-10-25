ee329dd67f2090426e7ebf546b3f5b806e43626d
jnaneshtngowda <114807226+jnaneshtngowda@users.noreply.github.com>
2022-10-25 14:00:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -53,7 +53,7 @@ FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).
 MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "True")), True)
 PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
 PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
-
+AUTO_DELETE_MESSAGE_TIME = environ.get('AUTO_DELETE_MESSAGE_TIME', '10')
 LOG_STR = "Current Cusomized Configurations are:-\n"
 LOG_STR += ("IMDB Results are enabled, Bot will be showing imdb details for you queries.\n" if IMDB else "IMBD Results are disabled.\n")
 LOG_STR += ("P_TTI_SHOW_OFF found , Users will be redirected to send /start to Bot PM instead of sending file file directly\n" if P_TTI_SHOW_OFF else "P_TTI_SHOW_OFF is disabled files will be send in PM, instead of sending start.\n")
@@ -66,8 +66,3 @@ LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
 
 SHORTENER_API = environ.get("SHORTENER_API", "d22152f4a53297536e1535f98593a30f1dadf8fe")
 LONG_DROPLINK_URL = environ.get("LONG_DROPLINK_URL", False)
-
-SELF_DELETE_SECONDS = int(environ.get('SELF_DELETE_SECONDS', 10))
-SELF_DELETE = environ.get('SELF_DELETE', True)
-if SELF_DELETE == "True":
-    SELF_DELETE = True
